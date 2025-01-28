7bbddb1e0f3fafe965f678a95cb7de7c705f0dbe
kaw393939 <kwilliam@njit.edu>
2025-01-28 14:55:12 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to your new React project!</h1>
+      <p>Start editing src/App.jsx to see your changes.</p>
+    </div>
+  )
+}
+
+export default App
